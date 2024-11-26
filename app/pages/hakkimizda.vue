3e2dcd4ac5bd05f901ac9a3c735f4346232a794e
feritcemkahraman <frtcm11@gmail.com>
2024-11-26 14:06:15 +0300

    yeni içerikler eklendi.

--- app/pages/hakkimizda.vue
+++ app/pages/hakkimizda.vue
@@ -1,8 +1,61 @@
-<script setup lang="ts"></script>
 <template>
-  <div>
-    <h1 class="font-bold text-2xl flex items-center justify-center h-screen">
-      Burası Hakkımızda
-    </h1>
+  <!--breadcrumb section start-->
+  <div class="bg-gray-800 py-20 px-4 md:px-8">
+    <div class="max-w-3xl mx-auto text-center">
+      <h2 class="text-3xl md:text-4xl font-bold text-white mb-4">
+        <p class="text-gray-400 text-lg">
+          YapıHan İnşaat Hakkında
+        </p>
+        Projelerimize, yenilikçi çözümler ve kapsamlı analizlerle yön veriyoruz.
+      </h2>
+    </div>
   </div>
+
+  <!--breadcrumb section end-->
+
+  <!--misyonumuz section start-->
+  <div class="container mx-auto my-32">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div>
+        <img src="/image1.jpg" alt="Modern House" class="w-full md:w-4/5 h-auto shadow-lg mx-auto" />
+      </div>
+      <div class="px-4 md:px-0 flex flex-col justify-center">
+        <p class="text-gray-600 font-semibold text-md pb-2">
+          MİSYONUMUZ
+        </p>
+        <h2 class="text-2xl font-bold mb-4">İnşaat Sektöründe Geleceği Şekillendiren Yapılar Oluşturmak</h2>
+        <p class="text-gray-600 mb-4">Yapıhan olarak vizyonumuz, inşaat sektöründe yenilikçi ve sürdürülebilir yaklaşımlarla geleceği şekillendiren lider bir marka olmaktır. Yaşam alanlarının sadece birer yapı değil, insanların hayatlarına değer katan mekanlar olduğunu bilincindeyiz. Bu nedenle, modern mimariye estetik tasarımlarla birleştirerek, fonksiyonel ve konforlu villalar inşa etmeyi hedefliyoruz.</p>
+        <div class="grid grid-cols-2 gap-4">
+          <div>
+            <h3 class="text-lg font-bold mb-2">Sürdürülebilir ve Çevre Dostu Projeler</h3>
+            <p class="text-gray-600">Doğaya saygılı ve çevre dostu projeler geliştirerek, geleceği nesille yaşanabilir bir dünya bırakmayı amaçlıyoruz. Enerji verimliği, yüksek, ekolojik dengesi gözeten yapılarımızla en iyi şekilde kullanarak, akıllı ve konforlu standart haline getirmeyi planlıyoruz.</p>
+          </div>
+          <div>
+            <h3 class="text-lg font-bold mb-2">Toplumsal Değerlere Katkı Sağlamak</h3>
+            <p class="text-gray-600">Toplumun ihtiyaçlarına duyarlı projeler üretererek, sosyal sorumluluk bilinciyie hareket ediyoruz. Eğitimden sağlığa, kültürden sanata çeşitli alanlarda destekleyici faaliyetlerde bulunarak, toplumsal gelişime katkı sağlamayı vizyonumuzun bir parçası olarak görüyoruz.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--misyonumuz section end-->
+
+  <!--fullwidth section start-->
+  <!--fullwidth section start-->
+  <div class="my-28">
+    <div class="relative w-full h-[200px] overflow-hidden">
+      <img src="/fw.jpg" alt="Full width background" class="w-full h-full object-cover">
+      <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <h2 class="text-white text-2xl md:text-4xl font-bold text-center px-4">
+          Her projede mükemmelliği hedefleyen bir yaklaşım benimsiyoruz.
+        </h2>
+      </div>
+    </div>
+  </div>
+  <!--fullwidth section end-->
+
 </template>
+
+<script>
+
+</script>
